ce54d5dcca28e4244b2c08fb2eac0ad4489b6372
Irvin Reyes <reyes.yerp@gmail.com>
2020-08-18 20:47:57 -0500

    Add delete listing handler and mutation

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-import { ListingsData } from "./types";
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  ListingsData,
+} from "./types";
 import { server } from "../../lib/api";
 
 const LISTINGS = `
@@ -16,7 +20,16 @@ query Listings {
     numOfBaths
     rating
   }
-}`;
+}
+`;
+
+const DELETE_LISTING = `
+mutation DeleteListing($id: ID!) {
+  deleteListing(id: $id) {
+    id
+  }
+}
+`;
 
 interface Props {
   title: string;
@@ -28,10 +41,24 @@ export const Listings = ({ title }: Props) => {
     console.log(data);
   };
 
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: "5f35ecc7d75bce09cce9ef74",
+      },
+    });
+    console.log(data);
+  };
+
   return (
     <div>
       <p>{title}</p>
-      <button onClick={fetchListings}>Query Listings!</button>
+      <button onClick={fetchListings}>Query listings!</button>
+      <button onClick={deleteListing}>Delete a listing!</button>
     </div>
   );
 };
