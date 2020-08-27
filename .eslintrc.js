974687c812e92f4ade7a1d1124d8e0d14923b065
Irvin Reyes <reyes.yerp@gmail.com>
2020-08-27 18:53:36 -0500

    Configure eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'react',
+    'import',
+    'jsx-a11y',
+    'react-hooks',
+    'simple-import-sort',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/prop-types': 0,
+    'no-console': 1,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 2,
+    'simple-import-sort/sort': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
